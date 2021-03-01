9b61af9c17ab292eb68bda0217b31afd7488c055
= <duhabars2@gmail.com>
2021-03-01 19:01:26 +0300

    StringRequest changed to JsonObjectRequest Data parsed and returned as QRModel object by JSON library

--- app/src/main/java/com/avalon/vflapp/ui/hub/QRViewModel.kt
+++ app/src/main/java/com/avalon/vflapp/ui/hub/QRViewModel.kt
@@ -7,30 +7,42 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.avalon.vflapp.ui.model.QRModel
 import com.avalon.vflapp.util.DataState
+import org.json.JSONArray
+import org.json.JSONObject
+import java.time.LocalDate
 
 private const val TAG = "QRViewModel"
 
 class QRViewModel(
     application: Application
 ) : AndroidViewModel(application) {
-    private val _code = MutableLiveData<DataState<String>>()
-    val code : LiveData<DataState<String>>
+    private val _code = MutableLiveData<DataState<QRModel>>()
+    val code : LiveData<DataState<QRModel>>
     get() = _code
 
     fun getQR() {
         val queue = Volley.newRequestQueue(getApplication())
 
-        val request = StringRequest(Request.Method.GET, "http://foravalon.com/getcode.php",
-            { res ->
-                _code.postValue(DataState.Success(res))
-                Log.d(TAG, "getQR: $res")
-            }, {
-                _code.postValue(DataState.Error(it))
-                Log.d(TAG, "getQR: $it")
-            })
+        val request = JsonObjectRequest("http://foravalon.com/getcode.php", null, {
+            val qrList = it.getJSONArray("qr")
+            val qr = qrList.get(0) as JSONObject
+            val qrModel = QRModel(
+                    code = qr.getString("code"),
+                    date = qr.getString("date"),
+            )
+
+            _code.postValue(DataState.Success(qrModel))
+            Log.d(TAG, "getQR: qrModel: $qrModel")
+        }, {
+            _code.postValue(DataState.Error(it))
+            Log.d(TAG, "getQR: $it")
+        })
 
         queue.add(request)
     }
