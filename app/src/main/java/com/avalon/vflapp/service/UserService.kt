be51f7267f63aa857c39c45fec25adc521d647c5
Duha Bars Okutan <duhabars2@gmail.com>
2021-03-07 14:56:51 +0300

    Add getRole function

--- app/src/main/java/com/avalon/vflapp/service/UserService.kt
+++ app/src/main/java/com/avalon/vflapp/service/UserService.kt
@@ -4,14 +4,17 @@ import com.avalon.vflapp.domain.User
 import com.avalon.vflapp.ui.model.AuthUser
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.GetTokenResult
 
-interface UserService {
+interface UserService  {
 
-    fun delete(users: List<User>): Task<String>
+    fun delete(users: List<User>): Task<Any>
 
-    fun add(user: User): Task<String>
+    fun add(user: User): Task<Any>
 
     fun getAll(): Any
 
     fun signIn(user: AuthUser): Task<AuthResult>
+
+    fun getRole(): Task<GetTokenResult>
 }
