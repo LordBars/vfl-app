f4277f2d94a9fc6bea731fe33f7e0295e597734f
Duha Bars Okutan <duhabars2@gmail.com>
2021-03-07 14:51:46 +0300

    Hilt-DI support, remove redundant calls and logs

--- app/src/main/java/com/avalon/vflapp/ui/hub/QRFragment.kt
+++ app/src/main/java/com/avalon/vflapp/ui/hub/QRFragment.kt
@@ -1,58 +1,53 @@
 package com.avalon.vflapp.ui.hub
 
-import android.content.Context.WINDOW_SERVICE
 import android.graphics.Point
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.avalon.vflapp.R
 import com.avalon.vflapp.databinding.FragmentQRBinding
 import com.avalon.vflapp.util.DataState
 import com.google.zxing.WriterException
-import kotlinx.coroutines.CoroutineScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.math.min
 
-private const val TAG = "QRFragment"
-
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
 class QRFragment : Fragment() {
 
     private var _binding: FragmentQRBinding? = null
 
     private val binding get() = _binding
 
-    private val viewModel: QRViewModel by viewModels()
+    private val viewModel: HubViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentQRBinding.inflate(inflater, container, false)
-        val view = binding?.root
         subscribeObservers()
-        binding?.qrSwipeRefresh?.isRefreshing = true
         viewModel.getQR()
         binding?.qrSwipeRefresh?.setOnRefreshListener {
-            viewModel.getQR()
+          viewModel.getQR()
         }
-        return view
+        return binding?.root
     }
 
     private fun subscribeObservers() {
-        viewModel.code.observe(requireActivity()) { dataState ->
+        viewModel.dataStateQR.observe(viewLifecycleOwner) { dataState ->
             when(dataState) {
                 is DataState.Success -> {
                     binding?.qrDateTv?.text = dataState.data.date
                     binding?.qrSwipeRefresh?.isRefreshing = false
                     createQR(dataState.data.code)
                 }
-                
+
                 is DataState.Error -> {
-                    Log.d(TAG, "subscribeObservers: ${dataState.exception}")
                     binding?.qrSwipeRefresh?.isRefreshing = false
                     toast("İstek başarısız")
                 }
@@ -60,6 +55,7 @@ class QRFragment : Fragment() {
                     binding?.qrSwipeRefresh?.isRefreshing = false
                     toast("İstek iptal edildi")
                 }
+                else ->  binding?.qrSwipeRefresh?.isRefreshing = true
             }
         }
     }
@@ -80,7 +76,6 @@ class QRFragment : Fragment() {
             binding?.qrContainer?.setImageBitmap(bitmap)
         }catch (e: WriterException) {
             toast("Karekod oluşturulamadı")
-            Log.d(TAG, "createQR: $e")
         }
 
     }
