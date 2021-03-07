a6ad77c54359d17d68b4ed381481c7bc6bbd71e8
Duha Bars Okutan <duhabars2@gmail.com>
2021-03-07 14:53:38 +0300

    @ExperimentalCoroutinesApi added

--- app/src/main/java/com/avalon/vflapp/di/RepositoryModule.kt
+++ app/src/main/java/com/avalon/vflapp/di/RepositoryModule.kt
@@ -1,19 +1,21 @@
 package com.avalon.vflapp.di
 
 import com.avalon.vflapp.service.UserService
-import com.avalon.vflapp.repository.LoginRepository
+import com.avalon.vflapp.repository.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RepositoryModule {
+    @ExperimentalCoroutinesApi
     @Singleton
     @Provides
-    fun provideLoginRepository(userService: UserService): LoginRepository {
-        return LoginRepository(userService)
+    fun provideLoginRepository(userService: UserService): UserRepository {
+        return UserRepository(userService)
     }
 }
