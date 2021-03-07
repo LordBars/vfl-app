f97f8d871d3971bc9bb6d1c66ac7e745e304a3cc
Duha Bars Okutan <duhabars2@gmail.com>
2021-03-07 14:58:59 +0300

    GSON converter for User object, add getRole function

--- app/src/main/java/com/avalon/vflapp/service/UserServiceImpl.kt
+++ app/src/main/java/com/avalon/vflapp/service/UserServiceImpl.kt
@@ -1,45 +1,47 @@
 package com.avalon.vflapp.service
 
-import android.util.Log
 import com.avalon.vflapp.domain.User
 import com.avalon.vflapp.ui.model.AuthUser
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GetTokenResult
 import com.google.firebase.functions.FirebaseFunctions
+import com.google.gson.Gson
 import javax.inject.Inject
-
-private const val TAG = "UserServiceImpl"
 
 class UserServiceImpl
 @Inject
 constructor(
-    private val functions: FirebaseFunctions,
-    private val auth: FirebaseAuth,
+        private val functions: FirebaseFunctions,
+        private val auth: FirebaseAuth,
 ): UserService {
 
-    override fun delete(users: List<User>): Task<String> {
+    override fun delete(users: List<User>): Task<Any> {
         return functions.getHttpsCallable("deleteUser").call(users).continueWith {
             task -> task.result?.data as String
         }
     }
 
-    override fun add(user: User): Task<String> {
-        return functions.getHttpsCallable("addUser").call(user).continueWith {
-            task -> task.result?.data as String
+    override fun add(user: User): Task<Any> {
+        val gUser = Gson().toJson(user)
+        return functions.getHttpsCallable("addUser").call(gUser).continueWith {
+            it.result?.data
         }
     }
 
     override fun getAll(): Task<Any> {
         return functions.getHttpsCallable("fetchUsers").call().continueWith {
-            Log.d(TAG, "getAll: ${it.result?.data}")
             it.result?.data
         }
     }
 
     override fun signIn(user: AuthUser): Task<AuthResult> {
-        Log.d(TAG, "signIn: Email: ${user.email}, Password: ${user.password}")
         return auth.signInWithEmailAndPassword(user.email, user.password)
+    }
+
+    override fun getRole(): Task<GetTokenResult> {
+        return auth.currentUser!!.getIdToken(true)
     }
 
 }
