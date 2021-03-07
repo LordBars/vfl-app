196f087e998465b41da00fe1582d313413252eb4
Duha Bars Okutan <duhabars2@gmail.com>
2021-03-07 14:50:31 +0300

    Hilt-DI support

--- app/src/main/java/com/avalon/vflapp/ui/hub/NoticeFragment.kt
+++ app/src/main/java/com/avalon/vflapp/ui/hub/NoticeFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.avalon.vflapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NoticeFragment : Fragment() {
 
     override fun onCreateView(
