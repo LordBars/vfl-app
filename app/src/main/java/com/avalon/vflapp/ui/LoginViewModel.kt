5e7bb4e38783ce39a43efbafbe24cd5074a17eda
Duha Bars Okutan <duhabars2@gmail.com>
2021-03-07 14:48:43 +0300

    Support for UserRepository.kt instead of old LoginRepository.kt

--- app/src/main/java/com/avalon/vflapp/ui/LoginViewModel.kt
+++ app/src/main/java/com/avalon/vflapp/ui/LoginViewModel.kt
@@ -1,33 +1,30 @@
 package com.avalon.vflapp.ui
 
 import androidx.lifecycle.*
-import com.avalon.vflapp.repository.LoginRepository
+import com.avalon.vflapp.repository.UserRepository
 import com.avalon.vflapp.ui.model.AuthUser
 import com.avalon.vflapp.util.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.math.log
 
+@ExperimentalCoroutinesApi
 @HiltViewModel
 class LoginViewModel
 @Inject
 constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val loginRepository: LoginRepository
+    private val userRepository: UserRepository
 ) : ViewModel() {
     private var _dataState: MutableLiveData<DataState<Boolean>> = MutableLiveData()
     val dataState: LiveData<DataState<Boolean>>
-    get() = _dataState
+        get() = _dataState
 
-    @ExperimentalCoroutinesApi
     fun signIn(user: AuthUser) {
         viewModelScope.launch {
-            loginRepository.signIn(user).collect {
+            userRepository.signIn(user).collect {
                 _dataState.postValue(it)
             }
         }
